fe6f09c3aed5b9472ebe07d5e7dbfe89b2e15f8a
aiporre <ariel.iporre.rivas@gmail.com>
2023-01-09 15:13:02 +0100

    start graph operations library in c++

---
+++ graphop/main.cpp
@@ -0,0 +1,11 @@
+// argument_definitions.cpp
+// compile with: /EHsc
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+int main( int argc, char *argv[])
+{
+    cout << "--- Start ---" << "\n";
+    return 0;
+}
