d8177b4044dbfbad1214a53b5815d75367fcf16c
aiporre <ariel.iporre.rivas@gmail.com>
2023-01-09 16:33:14 +0100

    implemented base methods for general Mesh class

--- spherepar/mesh.py
+++ spherepar/mesh.py
@@ -49,36 +49,75 @@ def get_segments(data, mask=None, num_segments=100):
     return segments
 
 class Vertex:
-    def __init__(self, pos, id):
+    def __init__(self, pos, _id):
         self.pos = pos
-        self.id = id
+        self.id = _id
 class Edge:
-    def __init__(self, u, v, id):
+    def __init__(self, u, v, _id):
         self.u = u
         self.v = v
-        self.id = id
+        self.id = _id
+    def get_vertex(self, v_id):
+        if v_id == self.u.id:
+            return self.u
+        elif v_id == self.v.id:
+            return self.v
+        else:
+            return None
+
 class Face:
-    def __init__(self, u, v, w, id):
+    def __init__(self, u, v, w, _id):
         self.u = u
         self.v = v
         self.w = w
-        self.id = id
+        self.id = _id
+        self._edges = None
+
+    def get_edge(self, v1_id, v2_id):
+        # create a list of edges
+        if self._edges is None:
+            triplet_vrtx = [self.u, self.v, self.w]
+            for a, b in zip(triplet_vrtx[:-1], triplet_vrtx[1:]):
+                if a.id > b.id:
+                    a, b = b, a
+                self._edges = {(a.id, b.id): (a, b)}
+        # look for the pair of ids
+        if v1_id > v2_id:
+            v1_id, v2_id = v2_id, v1_id
+        edge = self._edges.get((v1_id, v2_id), None)
+        return edge
 class Tetrahedron:
-    def __init__(self, u, v, w, m, id):
+    def __init__(self, u, v, w, m, _id):
         self.u = u
         self.v = v
         self.w = w
         self.m = m
-        self.id = id
+        self.id = _id
 class Mesh:
-    def get_vertex_neigbors(self, id):
-        raise NotImplementedError
 
-    def get_edge_faces(self, id):
-        raise NotImplementedError
+    def __init__(self, vertices, edges, faces, tetrahedra):
+        self.vertices = vertices
+        self.edges = edges
+        self.faces = faces
+        self.tetrahedra = tetrahedra
 
-    def get_edge(self, id):
-        raise NotImplementedError
+    def get_vertex_neighbors(self, v_id):
+        neighbors = []
+        for a, b in self.edges.keys():
+            if a == v_id:
+                neighbors.append(self.vertices[v_id])
+            if b == v_id:
+                neighbors.append(self.vertices[v_id])
+        return neighbors
+
+    def get_edge_faces(self, v_id):
+        edge = self.edges[v_id]
+        faces_with_edge = []
+        for face in self.faces:
+            if face.get_edge(edge.u._id, edge.v._id) is not None:
+                faces_with_edge.append(face)
+        return faces_with_edge
+
     def get_laplacian_matrix(self):
         raise NotImplementedError
 
@@ -86,33 +125,37 @@ class MeshVolume(Mesh):
     def __init__(self, *args, **kwargs):
         if "meshio_obj":
             meshio_obj = kwargs['meshio_obj']
-            self.vertices = {}
+            _vertices = {}
             for _id, v in enumerate(meshio_obj.points):
-                self.vertices[_id] = Vertex(v, _id)
+                _vertices[_id] = Vertex(v, _id)
 
             faces = [x for x in meshio_obj.cells if x.type == 'triangle'][0]
-            self.faces = {}
+            _faces = {}
             for _id, f in enumerate(faces.data):
-                u, v, w = self.vertices[f[0]], self.vertices[f[1]], self.vertices[f[2]]
-                self.faces[_id] = Face(u,v,w,_id)
+                u, v, w = self.vertices[f[0]], _vertices[f[1]], _vertices[f[2]]
+                _faces[_id] = Face(u,v,w,_id)
 
             tetras = [x for x in meshio_obj.cells if x.type == 'tetras'][0]
-            self.tetrahedra = {}
+            _tetrahedra = {}
             for _id, t in enumerate(tetras.data):
-                u, v, w, m = self.vertices[f[0]], self.vertices[f[1]], self.vertices[f[2]], self.vertices[f[3]]
-                self.faces[_id] = Face(u,v,w, m, _id)
+                u, v, w, m = _vertices[f[0]], _vertices[f[1]], _vertices[f[2]], _vertices[f[3]]
+                _faces[_id] = Face(u,v,w, m, _id)
 
             # extract edges from
-            self.edges = {}
+            _edges = {}
             _id = 0
             for tetra in tetras:
                 for a, b in zip(tetra[:-1], tetra[1:]):
                     if a>b:
                         a, b = b, a
-                    if (a,b) not in self.edges:
-                        v1, v2 = self.vertices[a], self.vertices[b]
-                        self.edges[(a,b)] = (v1,v2)
+                    if (a,b) not in _edges:
+                        v1, v2 = _vertices[a], _vertices[b]
+                        _edges[(a,b)] = (v1,v2)
 
+    def get_vertex_neighbors(self, v_id):
+        for a, b in self.edges.keys():
+            if a == v_id:
+                return self.vertices[v_id]
 
 
 
